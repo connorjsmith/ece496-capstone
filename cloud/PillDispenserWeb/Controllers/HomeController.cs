c73d9bbe98e200982ead4bfe14db83c27fdb95e9
Connor J Smith <connorjsmith101@gmail.com>
2017-07-18 21:42:23 -0700

    Updated gitignore, added more region definitions

--- cloud/PillDispenserWeb/Controllers/HomeController.cs
+++ cloud/PillDispenserWeb/Controllers/HomeController.cs
@@ -11,16 +11,24 @@ namespace PillDispenserWeb
     [Route("")]
     public class HomeController : Controller
     {
+        #region Homepage Routes
+
         [HttpGet("")]
         public async Task<IActionResult> Index()
         {
             return View("/Views/Home/Index.cshtml");
         }
 
+        #endregion Homepage Routes
+
+        #region Example Secondary Route
+
         [HttpGet("this/is/a/path")]
         public async Task<IActionResult> Other()
         {
             return View("/Views/Other/Other.cshtml");
         }
+
+        #endregion Example Secondary Route
     }
 }
