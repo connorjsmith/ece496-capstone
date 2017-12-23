e33c5ea961021f2efc14eaa49542e6e8989e8de2
Connor Smith <connorjsmith101@gmail.com>
2017-12-23 13:26:43 -0500

    Added Heartbeat endpoint for tracking connected devices

---
+++ cloud/PillDispenserWeb.Tests/Controllers/HeartbeatControllerTest.cs
@@ -0,0 +1,72 @@
+﻿using Moq;
+using NUnit.Framework;
+using PillDispenserWeb.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PillDispenserWeb.Tests.Controllers
+{
+    [TestFixture]
+    class HeartbeatControllerTest
+    {
+        #region Controller and Mocks
+        private HeartbeatController hbController;
+        private SortedSet<string> controllerLastIterationDeviceIds;
+        private SortedSet<string> controllerCurrentIterationDeviceIds;
+        private SortedSet<string> controllerLastMissingDeviceIds;
+        #endregion
+
+        #region Setup and Teardown
+        [SetUp]
+        public void Setup()
+        {
+            hbController = new HeartbeatController();
+        }
+        #endregion
+
+        #region Tests
+        [Test]
+        public void Heartbeat_AddsToCurrentSet()
+        {
+            hbController.HeartbeatFromDevice("1");
+            hbController.HeartbeatFromDevice("2");
+            hbController.HeartbeatFromDevice("3");
+            Assert.That(hbController.CurrentIterationDeviceIds.Contains("1"));
+            Assert.That(hbController.CurrentIterationDeviceIds.Contains("2"));
+            Assert.That(hbController.CurrentIterationDeviceIds.Contains("3"));
+            Assert.AreEqual(hbController.CurrentIterationDeviceIds.Count, 3);
+
+            hbController.HeartbeatTask();
+            Assert.That(hbController.LastIterationDeviceIds.Contains("1"));
+            Assert.That(hbController.LastIterationDeviceIds.Contains("2"));
+            Assert.That(hbController.LastIterationDeviceIds.Contains("3"));
+            Assert.AreEqual(hbController.LastIterationDeviceIds.Count, 3);
+            Assert.IsEmpty(hbController.CurrentIterationDeviceIds);
+
+            hbController.HeartbeatFromDevice("1");
+            hbController.HeartbeatFromDevice("2");
+            Assert.That(hbController.CurrentIterationDeviceIds.Contains("1"));
+            Assert.That(hbController.CurrentIterationDeviceIds.Contains("2"));
+            Assert.AreEqual(hbController.CurrentIterationDeviceIds.Count, 2);
+            Assert.AreEqual(hbController.LastIterationDeviceIds.Count, 3);
+
+            hbController.HeartbeatTask();
+            Assert.That(hbController.LastIterationDeviceIds.Contains("1"));
+            Assert.That(hbController.LastIterationDeviceIds.Contains("2"));
+            Assert.AreEqual(hbController.LastIterationDeviceIds.Count, 2);
+            Assert.That(hbController.LastMissingDeviceIds.Contains("3"));
+            Assert.AreEqual(hbController.LastMissingDeviceIds.Count, 1);
+
+            hbController.HeartbeatTask();
+            Assert.That(hbController.LastMissingDeviceIds.Contains("1"));
+            Assert.That(hbController.LastMissingDeviceIds.Contains("2"));
+            Assert.AreEqual(hbController.LastMissingDeviceIds.Count, 2);
+            Assert.IsEmpty(hbController.CurrentIterationDeviceIds);
+            Assert.IsEmpty(hbController.LastIterationDeviceIds);
+
+        }
+
+        #endregion
+    }
+}
---
+++ cloud/PillDispenserWeb/Controllers/HeartbeatController.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
+
+namespace PillDispenserWeb.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/Heartbeat")]
+    public class HeartbeatController : Controller
+    {
+        public SortedSet<string> LastMissingDeviceIds;
+        public SortedSet<string> LastIterationDeviceIds;
+        public SortedSet<string> CurrentIterationDeviceIds;
+
+        public HeartbeatController(SortedSet<string> lastMissing = null, SortedSet<string> lastIteration = null, SortedSet<string> currentIteration = null)
+        {
+            LastMissingDeviceIds = lastMissing ?? new SortedSet<string>();
+            LastIterationDeviceIds = lastIteration ?? new SortedSet<string>();
+            CurrentIterationDeviceIds = currentIteration ?? new SortedSet<string>();
+        }
+        public void HeartbeatTask()
+        {
+            lock (CurrentIterationDeviceIds)
+            {
+                // Missing twice in a row = lastMissing - currentIteration
+                var missedTwo = new SortedSet<string>(LastMissingDeviceIds.Except(CurrentIterationDeviceIds));
+                // TODO: notify all the missedTwo patients
+
+                // first time missing = lastIteration - currentIteration
+                LastMissingDeviceIds = new SortedSet<string>(LastIterationDeviceIds.Except(CurrentIterationDeviceIds));
+
+                // newly added = currentIteration - lastIteration
+                // TODO: necessary?
+
+                // lastIteration = currentIteration
+                LastIterationDeviceIds = new SortedSet<string>(CurrentIterationDeviceIds);
+                CurrentIterationDeviceIds.Clear();
+            }
+
+        }
+
+        [HttpPost]
+        [Route("/")]
+        public JsonResult HeartbeatFromDevice(string deviceId)
+        {
+            // TODO some sort of secret key to better authenticate these endpoints?
+            lock (CurrentIterationDeviceIds)
+            {
+                CurrentIterationDeviceIds.Add(deviceId);
+            }
+            return Json("success");
+        }
+    }
+}
